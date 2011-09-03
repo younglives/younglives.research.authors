e5ec220524b51c8d76a3662d5a37d44a8d899024
Michael Davis <m.r.davis@me.com>
2011-09-03 15:42:13 +0000

    Point to the versions file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open(os.path.join("younglives", "research", "types", "version.txt")).read().strip()
 
 setup(name='younglives.research.authors',
       version=version,
