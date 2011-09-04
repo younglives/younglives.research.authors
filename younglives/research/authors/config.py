0e7614740f08007c7d75f1dd7ef897873ce9186d
Michael Davis <m.r.davis@me.com>
2011-09-04 11:31:15 +0000

    Add the names vocab for the import

--- younglives/research/authors/config.py
+++ younglives/research/authors/config.py
@@ -6,3 +6,18 @@ ADD_PERMISSIONS = {
     'Author': 'YounglivesResearchAuthors: Add Author',
     'AuthorFolder': 'YounglivesResearchAuthors: Add AuthorFolder',
 }
+
+# vocabs for initial imports
+
+VIETNAM_NAMES = DisplayList((
+    ('Le', 'Thuc Duc'),
+    ('Vu', 'Hoang Dat'),
+    ('Nguyen', 'Hoai Chau'),
+    ('Truong', 'Huyen Chi'),
+    ('Nguyen', 'Ngoc Danh'),
+    ('Nguyen', 'Ngoc P.'),
+    ('Tran', 'Ngoc Ai Vy'),
+    ('Vu', 'Thi Thanh Huong'),
+    ('Tran', 'Ngo Minh Tam'),
+    ('Lopez Boo', 'Florencia'),
+))
